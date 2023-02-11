e433f5c83d0b7799ca0474a89ff54d833d701b4b
go-numb66 <gonumb66@gmail.com>
2023-02-12 05:49:59 +0900

    add speackers list

--- _examples/speaking/main.go
+++ _examples/speaking/main.go
@@ -2,15 +2,24 @@ package main
 
 import (
 	"fmt"
-	"github.com/go-numb/go-voicevox"
 	"log"
+	"time"
+
+	"github.com/go-numb/go-voicevox"
+)
+
+var (
+	texts = []string{
+		"萌え袖って……いいよねぇ︎",
+		"最近 CM ではめでたく? ノーリミットになってました",
+	}
 )
 
 func main() {
 	client := voicevox.New()
 
-	speakers := client.GetSpeakers()
-	if speakers == nil {
+	speakers, err := client.GetSpeakers()
+	if err != nil {
 		log.Fatal("responce is nil")
 	}
 
@@ -18,28 +27,32 @@ func main() {
 		fmt.Println(speakers[i])
 	}
 
-	spk := speakers[client.Speaker]
+	spk := speakers[voicevox.NURSEROBOT]
 	if client.Style >= len(spk.Styles) {
 		log.Fatal("style not found")
 	}
 	spkID := spk.Styles[client.Style].ID
 	fmt.Println(spk.Name, spk.Styles[client.Style].Name, spkID)
 
-	params, err := client.GetQuery(spkID, "テストしています")
-	if err != nil {
-		log.Fatal(err)
-	}
+	for i := 0; i < len(texts); i++ {
+		now := time.Now()
+		params, err := client.GetQuery(spkID, texts[i])
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	client.Set(params)
-	fmt.Printf("%#v\n", params)
+		client.Set(params)
+		fmt.Printf("%#v\n", params)
 
-	b, err := client.Synth(spkID, params)
-	if err != nil {
-		log.Fatal(err)
-	}
+		b, err := client.Synth(spkID, params)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("処理時間: %.3fs\n", time.Since(now).Seconds())
 
-	if err := client.Speaking(params, b[44:]); err != nil {
-		log.Fatal(err)
+		if err := client.Speaking(params, b[44:]); err != nil {
+			log.Fatal(err)
+		}
 	}
 
 }
